2aa51da4c91dc5ae4f25f61be48599ba83282f12
bejordae <bej9@cornell.edu>
2026-02-01 10:11:28 -0500

    Update description

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -8,13 +8,17 @@ function ImageSection() {
             <div className="image-content">
                 <div className="about-info">
                     <p className="about-text">
-                        <span>I am a software engineer at Amazon and a graduate of Cornell & RIT CS.</span>
+                        <span>I am an engineer at Amazon and a graduate of Cornell & RIT CS.</span>
                         <br/><br/>
-                        In my current job, I primarily work on large scale spark ETL jobs, ML ops, and other fullstack tasks.
-                        I have a background in ML from my masters as well as from my previous role and internship.
+                        In my current role, I build ML infrastructure and large-scale Spark ETL pipelines
+                        to support production ML systems. My team's MLOps framework based on AWS SageMaker
+                        became a template used for multiple other ML projects throughout the org.
                         <br/><br/>
-                        Outside of work I like to spend my time on my project, Entropy Audio, which aims to redefine how we create sound for music composition.
-                        I also love trying new food, cool architecture, and keeping up with research in CS.
+                        Previously I worked as an MLE at Northrop Grumman and as an MLE intern at KLA, focusing on deep computer vision applications as well as information retrieval.
+                        <br/><br/>
+                        Outside of work, I'm building Entropy Audio, which aims to redefine how we design sound.
+                        I am training and aligning diffusion models for professional sound design.
+                        I also enjoy architecture, trying new food, and keeping up with research on diffusion models and similar topics.
                     </p>
                 </div>
                 <div className="about-img-wrapper">
